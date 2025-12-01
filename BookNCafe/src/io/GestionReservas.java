a00e25fc71744813e82590fbe7e5b1bbd8b2c378
Irene Olga Laiseca <ireneolga.l.h@opendeusto.es>
2025-12-01 08:53:37 +0100

    Creacion de clase gestionReservas

---
+++ BookNCafe/src/io/GestionReservas.java
@@ -0,0 +1,25 @@
+package io;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class GestionReservas {
+	public static final Path ruta_archivo_reservas = Paths.get("resources", "data", "reservas.csv");
+	
+	// Gestionar reservas
+	public void gestionClientes() {
+		
+	}
+	
+	// Guardar reservas
+	public void guardarReservas() {
+		
+	}
+	
+	// Cargar reservas
+	public void cargarReservas() {
+		
+	}
+	
+	
+}
