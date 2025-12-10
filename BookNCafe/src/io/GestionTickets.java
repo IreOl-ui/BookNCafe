c01ba04c0aa2d1efa22574db076b257dcad66bd8
Irene Olga Laiseca <ireneolga.l.h@opendeusto.es>
2025-12-10 10:47:46 +0100

    Actualizacion general

---
+++ BookNCafe/src/io/GestionTickets.java
@@ -0,0 +1,10 @@
+package io;
+
+public class GestionTickets {
+
+	public static void guardarTicketReservaLibro(String isbn, String titulo, String autor, String nombreCliente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
