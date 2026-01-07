9d568debedd9bef2802d80ba7c01825dec221ff9
ggredilla <gaizka.gredilla@opendeusto.es>
2026-01-07 10:58:00 +0100

    Filtros e inicio sesión cafetería

--- BookNCafe/src/domain/Cafeteria.java
+++ BookNCafe/src/domain/Cafeteria.java
@@ -41,4 +41,53 @@ public class Cafeteria {
         }
         return encontrados;
     }
+    public List<Producto> filtrarSinAlergeno(Alergeno a) {
+        List<Producto> aptos = new ArrayList<>();
+        for (Producto p : inventario) {
+            if (!p.getAlergeno().contains(a)) {
+                aptos.add(p);
+            }
+        }
+        return aptos;
+    }
+
+    public List<Producto> filtrarPorPrecioMaximo(double precioMax) {
+        List<Producto> baratos = new ArrayList<>();
+        for (Producto p : inventario) {
+            if (p.getPrecio() <= precioMax) {
+                baratos.add(p);
+            }
+        }
+        return baratos;
+    }
+
+
+    public double calcularPrecioMedio() {
+        if (inventario.isEmpty()) return 0.0;
+        double suma = 0;
+        for (Producto p : inventario) {
+            suma += p.getPrecio();
+        }
+        return suma / inventario.size();
+    }
+
+    public Producto obtenerProductoMasCaro() {
+        if (inventario.isEmpty()) return null;
+        Producto masCaro = inventario.get(0);
+        for (Producto p : inventario) {
+            if (p.getPrecio() > masCaro.getPrecio()) {
+                masCaro = p;
+            }
+        }
+        return masCaro;
+    }
+
+
+    public void setUsuarioIdentificado(Cliente c) {
+        this.usuarioIdentificado = c;
+    }
+
+    public Cliente getUsuarioIdentificado() {
+        return usuarioIdentificado;
+    }
 }
