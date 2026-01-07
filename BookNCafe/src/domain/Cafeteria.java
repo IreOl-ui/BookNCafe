f6933c0b22e4d0affa6fa6b2cc8335669445fb59
ggredilla <gaizka.gredilla@opendeusto.es>
2026-01-07 10:55:49 +0100

    Atributos y constructores cafetería

--- BookNCafe/src/domain/Cafeteria.java
+++ BookNCafe/src/domain/Cafeteria.java
@@ -1,5 +1,26 @@
 package domain;
 
-public class Cafeteria {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cafeteria {
+    private List<Producto> inventario;
+    private List<Cliente> listaClientes;
+    private Cliente usuarioIdentificado;
+
+    public Cafeteria() {
+        this.inventario = new ArrayList<>();
+        this.listaClientes = new ArrayList<>();
+        this.usuarioIdentificado = null;
+    }
+
+    public void añadirProducto(Producto p) {
+        if (p != null) {
+            this.inventario.add(p);
+        }
+    }
+
+    public List<Producto> getInventario() {
+        return inventario;
+    }
 }
