8e798a06845b887bf1b95a1c4a5b5ecf4343f469
ggredilla <gaizka.gredilla@opendeusto.es>
2026-01-07 10:56:43 +0100

    Metodos cafetería

--- BookNCafe/src/domain/Cafeteria.java
+++ BookNCafe/src/domain/Cafeteria.java
@@ -23,4 +23,22 @@ public class Cafeteria {
     public List<Producto> getInventario() {
         return inventario;
     }
+    public Producto buscarProductoPorNombre(String nombre) {
+        for (Producto p : inventario) {
+            if (p.getNombre().equalsIgnoreCase(nombre)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public List<Producto> buscarPorPersonaje(String personaje) {
+        List<Producto> encontrados = new ArrayList<>();
+        for (Producto p : inventario) {
+            if (p.getPersonaje().toLowerCase().contains(personaje.toLowerCase())) {
+                encontrados.add(p);
+            }
+        }
+        return encontrados;
+    }
 }
